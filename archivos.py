08cfe09b744d983a1447ed25a27d8a5591dc6b95
JavierRamirez20 <61593926+JavierRamirez20@users.noreply.github.com>
2023-11-03 17:06:02 -0600

    Subiendo...

---
+++ archivos.py
@@ -0,0 +1,67 @@
+# Source Generated with Decompyle++
+# File: archivos.pyc (Python 3.9)
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+
+class App(Tk):
+    
+    def __init__(self):
+        super().__init__()
+        self.geometry("400x300")
+        self.title("Manipula archivos")
+        self.barra_de_menus = Menu(self)
+        self.config(menu=self.barra_de_menus)
+        self.menu1 = Menu(self.barra_de_menus, tearoff=0)
+        self.menu1.add_command(label="Nuevo", command=self.nuevo)
+        self.menu1.add_command(label="Abrir", command=self.abrir)
+        self.menu1.add_command(label="Guardar como", command=self.guardar)
+        self.menu1.add_separator()
+        self.menu1.add_command(label="Cerrar", command=self.quit)
+        self.menu2 = Menu(self.barra_de_menus, tearoff=0)
+        self.menu2.add_command(label="Version", command=self.version)
+        self.menu2.add_command(label="Sobre mi", command=self.sobre)
+        self.barra_de_menus.add_cascade(label="Archivo", menu=self.menu1)
+        self.barra_de_menus.add_cascade(label="Ayuda", menu=self.menu2)
+        self.scrollv = Scrollbar(self)
+        self.scrollv.pack(side= RIGHT,fill="y")
+        self.scrollh = Scrollbar(self, orient= HORIZONTAL)
+        self.scrollh.pack(side= BOTTOM, fill= "x")
+        self.texto = Text(self, height= 500, width= 350, yscrollcommand= self.scrollv.set, xscrollcommand = self.scrollh.set, wrap= NONE)
+        self.texto.pack(fill = BOTH, expand=0)
+
+    
+    def nuevo(self):
+        self.texto.delete(1.0, END)
+
+    
+    def abrir(self):
+        selectordearchivo = filedialog.askopenfile(filetypes=(('text files', '*.txt'), ('All files', '*.*')))
+        if selectordearchivo is not None:
+            archivo = open(selectordearchivo.name, 'r', encoding='utf-8')
+            self.texto.delete(1.0, END)
+            self.texto.insert(END, archivo.read())
+            archivo.close()
+
+    
+    def guardar(self):
+        selectordearchivo = filedialog.asksaveasfile(filetypes=(('text files', '*.txt'),('All files', '*.*')), defaultextentions='.txt',mode='w')
+        if selectordearchivo is not None:
+            archivo = open(selectordearchivo.name, 'w', encoding='utf-8')
+            archivo.write(self.texto.get(1.0, END))
+            archivo.close()
+
+    
+    def sobre(self):
+        messagebox.showinfo(title='Sobre mi', message='Creado por: RA201803')
+
+    
+    def version(self):
+        messagebox.showinfo(title='Version', message='Version: 2.0')
+
+    __classcell__ = None
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
---
+++ login.py
@@ -0,0 +1,9 @@
+# Source Generated with Decompyle++
+# File: login.pyc (Python 3.9)
+
+usuario = input('Ingrese usuario: ')
+passwd = input('Ingrese contraseña: ')
+if usuario == 'RA201803' and passwd == 'RA201803123':
+    print('Credenciales Validas')
+else:
+    print('NO VALIDO')
